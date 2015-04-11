db9eaf60ad63d7aaaa6bb8b362943adb6c4fa70e
Mike <mike.l.holford@gmail.com>
2015-04-11 11:24:31 +0100

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,8 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).on("click", ".open-AddBookDialog", function () {
+ var myBookId = $(this).data('id');
+ $(".modal-body #bookId").val( myBookId );
+});
