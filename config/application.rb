4b2af524a88d3bc526ee5d3a537dff885e2f2355
Mike <mike.l.holford@gmail.com>
2015-04-12 09:58:53 +0100

    changes too app.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Codeandbeers
   class Application < Rails::Application
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,5 +25,3 @@ module Codeandbeers
   end
 end
 
-config.middleware.use ActionDispatch::Cookies
-config.middleware.use ActionDispatch::Session::CookieStore
